725a020259566fc27733b91df8f05e337e3aa8ee
rajkotiyanuman <rajkotiyamonuman1718.scoe@sinhgad.edu>
2021-03-11 14:45:12 +0530

    Add Script

---
+++ script/contact.js
@@ -0,0 +1,7 @@
+function Submit() {
+    alert("Thank you, I'll get in touch with you soon !!!");
+    document.getElementById('name').value ='';
+    document.getElementById('email').value ='';
+    document.getElementById('subject').value ='';
+    document.getElementById('msg').value ='';
+  }
