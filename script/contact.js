641749de28deb5bffe4d78f3cb8bb6113ce4a786
rajkotiyanuman <rajkotiyamonuman1718.scoe@sinhgad.edu>
2021-03-13 09:58:05 +0530

    Fix Contact Form

--- script/contact.js
+++
@@ -1,7 +0,0 @@
-function Submit() {
-    alert("Thank you, I'll get in touch with you soon !!!");
-    document.getElementById('name').value ='';
-    document.getElementById('email').value ='';
-    document.getElementById('subject').value ='';
-    document.getElementById('msg').value ='';
-  }
